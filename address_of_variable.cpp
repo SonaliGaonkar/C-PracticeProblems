f6428936085451b91c9e2cbf2b50fd85918acd9f
SonaliGaonkar <sonali.gaonkar@cctech.co.in>
2022-10-13 15:24:29 +0530

    commit

---
+++ address_of_variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    int a = 1;
+    int b = 756;
+    int c = 84747;
+    int d = 0;
+    int e =565;
+    int f =56;
+    std::cout << &a << "\n";
+    std::cout << &b << "\n";
+    std::cout << &c << "\n";
+    std::cout << &d << "\n";
+    std::cout << &e << "\n";
+    std::cout << &f << "\n";
+    return 0;
+}
--- structure.cpp
+++ structure.cpp
@@ -7,21 +7,31 @@ struct employee
     char address[20];
     float salary;
 };
+employee getData(employee E)
+{
+
+    std::cout << "Enter name of employee : ";
+    std::cin.get(E.name, 50);
+    std::cout << "\nEnter age of employee : ";
+    std::cin >> E.age;
+    std::cout << "\nEnter address of employee : ";
+    std::cin >> E.address;
+    std::cout << "\nEnter salary of employee : ";
+    std::cin >> E.salary;
+    return E;
+}
+void displayData(employee E)
+{
+    std::cout << "\nName of employee : " << E.name;
+    std::cout << "\nAge of employee : " << E.age;
+    std::cout << "\nAddress of employee : " << E.address;
+    std::cout << "\nSalary of employee : " << E.salary;
+}
 int main()
 {
-    employee e1;
-    std::cout << "Enter name of employee : ";
-    std::cin.get(e1.name, 50);
-    std::cout << "\nEnter age of employee : ";
-    std::cin >> e1.age;
-    std::cout << "\nEnter address of employee : ";
-    std::cin >> e1.address;
-    std::cout << "\nEnter salary of employee : ";
-    std::cin >> e1.salary;
-
-    std::cout << "\nName of employee : " << e1.name;
-    std::cout << "\nAge of employee : " << e1.age;
-    std::cout << "\nAddress of employee : " << e1.address;
-    std::cout << "\nSalary of employee : " << e1.salary;
+    employee E, temp;
+    temp = getData(E);
+    E = temp;
+    displayData(E);
     return 0;
 }
